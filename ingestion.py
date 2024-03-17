20064f74ca47b79bc6a3716bd346893a79ff4233
Soya AOKI <45820850+soyaoki@users.noreply.github.com>
2024-03-17 23:32:21 +0900

    update: step1 deliverables.

--- ingestion.py
+++ ingestion.py
@@ -4,7 +4,7 @@ import os
 import json
 from datetime import datetime
 
-
+import glob
 
 
 #############Load config.json and get input and output paths
@@ -19,8 +19,27 @@ output_folder_path = config['output_folder_path']
 #############Function for data ingestion
 def merge_multiple_dataframe():
     #check for datasets, compile them together, and write to an output file
-
-
-
+    
+    # Define dataframe
+    df_compiled = pd.DataFrame([])
+    
+    # Get paths of CSV files
+    csv_path_list = glob.glob(os.path.join(input_folder_path, '*.csv'))
+    
+    # Load each CSV file as dataframe
+    for csv_path in csv_path_list:
+        df = pd.read_csv(csv_path)
+        df_compiled = df_compiled.append(df, ignore_index=True)
+    
+    # Drop duplicates
+    df_compiled = df_compiled.drop_duplicates().reset_index(drop=True)
+    
+    # Save dataframe
+    df_compiled.to_csv(os.path.join(output_folder_path, 'finaldata.csv'), index=False)
+    
+    # Save names of CSV files
+    with open(os.path.join(output_folder_path, 'ingestedfiles.txt'), 'w') as f:
+        f.write('\n'.join(csv_path_list))
+    
 if __name__ == '__main__':
     merge_multiple_dataframe()
