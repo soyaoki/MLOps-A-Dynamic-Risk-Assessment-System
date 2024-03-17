054fa5e2dca29fdd27edcea86b157deb512fda85
Soya AOKI <45820850+soyaoki@users.noreply.github.com>
2024-03-18 00:18:22 +0900

    update: step2 deliverables.

--- scoring.py
+++ scoring.py
@@ -14,12 +14,26 @@ import json
 with open('config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
-
-
 #################Function for model scoring
-def score_model():
+def score_model(model_path, test_data_path):
     #this function should take a trained model, load test data, and calculate an F1 score for the model relative to the test data
     #it should write the result to the latestscore.txt file
+    
+    model = pickle.load(open(os.path.join(model_path, 'trainedmodel.pkl'), 'rb'))
+    
+    df_test = pd.read_csv(os.path.join(test_data_path, 'testdata.csv'))
+    X_test= df_test[['lastmonth_activity', 'lastyear_activity', 'number_of_employees']]
+    y_test = df_test['exited']
 
+    y_pred = model.predict(X_test)
+    
+    f1_score = metrics.f1_score(y_test, y_pred)
+    
+    with open(os.path.join(model_path, 'latestscore.txt'), 'w') as f:
+        f.write(str(f1_score))
+
+if __name__ == '__main__':
+    model_path = os.path.join(config['output_model_path']) 
+    test_data_path = os.path.join(config['test_data_path']) 
+    
+    score_model(model_path, test_data_path)
--- training.py
+++ training.py
@@ -12,21 +12,27 @@ import json
 with open('config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-model_path = os.path.join(config['output_model_path']) 
-
-
 #################Function for training the model
-def train_model():
+def train_model(dataset_csv_path, model_path):
     
     #use this logistic regression for training
-    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
-                    intercept_scaling=1, l1_ratio=None, max_iter=100,
-                    multi_class='warn', n_jobs=None, penalty='l2',
-                    random_state=0, solver='liblinear', tol=0.0001, verbose=0,
-                    warm_start=False)
+    model_lr = LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
+                                  intercept_scaling=1, l1_ratio=None, max_iter=100,
+                                  multi_class='auto', n_jobs=None, penalty='l2',
+                                  random_state=0, solver='liblinear', tol=0.0001, verbose=0,
+                                  warm_start=False)
     
     #fit the logistic regression to your data
+    df_train = pd.read_csv(os.path.join(dataset_csv_path, 'finaldata.csv'))
+    X_train= df_train[['lastmonth_activity', 'lastyear_activity', 'number_of_employees']]
+    y_train = df_train['exited']
+    model_lr.fit(X_train, y_train)
     
     #write the trained model to your workspace in a file called trainedmodel.pkl
-
+    pickle.dump(model_lr, open(os.path.join(model_path, 'trainedmodel.pkl'), 'wb'))
+    
+if __name__ == '__main__':
+    dataset_csv_path = os.path.join(config['output_folder_path']) 
+    model_path = os.path.join(config['output_model_path']) 
+    
+    train_model(dataset_csv_path, model_path)
